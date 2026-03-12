b4694651608eb9333c888c22de1e73b15dd8a658
Liam Walker <liamwalker@liamwalker.me>
2026-03-12 05:03:54 -0700

    Updates

--- src/pages/Miracosta.tsx
+++ src/pages/Miracosta.tsx
@@ -3,7 +3,7 @@ export default function Miracosta() {
     <div>
       <section className="title-hero" style={{ background: '#e5f5ff' }}>
         <div className="container" style={{ textAlign: 'center' }}>
-          <h1 className="hero-title">MiraCosta College</h1>
+          <h1 className="hero-title">MiraCosta !College</h1>
           <p className="hero-subtitle" style={{ marginTop: 12 }}>
             Work in Progress
           </p>
